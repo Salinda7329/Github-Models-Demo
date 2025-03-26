6714f2b1c89ed741a21cbe10a9a633ef8c1dea79
SalindaGIT <salindakas@gmail.com>
2025-03-26 13:24:48 +0530

    sample.js working

--- sample.js
+++ sample.js
@@ -1,21 +1,22 @@
-import 'dotenv/config';
 import OpenAI from "openai";
 
-const token = process.env.GITHUB_TOKEN;
+// const token =
 const endpoint = "https://models.inference.ai.azure.com";
-const modelName = "o3-mini";
+const modelName = "gpt-4o";
 
 export async function main() {
-
   const client = new OpenAI({ baseURL: endpoint, apiKey: token });
 
   const response = await client.chat.completions.create({
-     messages: [
-        { role:"developer", content: "You are a helpful assistant." },
-        { role:"user", content: "What is the capital of France?" }
-      ],
-      model: modelName
-    });
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: "What can we do with Git Hub Models? Provide a 20 word description" },
+    ],
+    temperature: 1.0,
+    top_p: 1.0,
+    max_tokens: 1000,
+    model: modelName,
+  });
 
   console.log(response.choices[0].message.content);
 }
